07b3b5788c4b4a5f7a647e035adbfa21233c2fac
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 13:58:23 -0800

    wayne: Use 4GB dalvik-heap config
    We are currently using 2GB dalvik-heap config.
    
    Bug: 145823510
    Test: MPTS test
    Change-Id: Ie26b8e4fefea25fa818bd1eaf9df4a0bb153d42d

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
