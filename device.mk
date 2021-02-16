286f7574b77cb5558f7ac349f8ad1135c9fe1f6f
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    wayne: Migrate to Xiaomi power AIDL HAL
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- device.mk
+++ device.mk
@@ -344,9 +344,11 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2.vendor \
-    android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.2.vendor
+    android.hardware.power-service.xiaomi-libperfmgr
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # Protobuf
 PRODUCT_PACKAGES += \
