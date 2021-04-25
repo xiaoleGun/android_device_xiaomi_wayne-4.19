2cba7d5db5f2d2c6fbfff6adc666319d83472142
Bruno Martins <bgcngm@gmail.com>
2021-04-25 14:18:08 +0100

    wayne: Sync boot image header with stock
    Pass proper boot image header version and at the same time
    ditch defaults.
    
    Change-Id: If0345731ab8edc9a7f4c2c506e3748833d6dd076

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,10 +76,12 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 BOARD_USES_METADATA_PARTITION := true
 
 # Kernel
+BOARD_BOOT_HEADER_VERSION := 1
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += printk.devkmsg=on
 BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
