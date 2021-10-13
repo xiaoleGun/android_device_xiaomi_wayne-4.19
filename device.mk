b087efd0121ec700fbc6cd927d02dce2507a5f72
orges <me@orgesified.ml>
2021-10-13 16:39:40 +0000

    wayne: add display config file
     * DisplayPowerController[0]: Screen brightness nits configuration is unavailable; falling back
     * DisplayDeviceConfig: requesting nits when no mapping exists.
    
    Signed-off-by: orges <me@orgesified.ml>
    Signed-off-by: xiaoleGun <1592501605@qq.com>
    Change-Id: Ieb69a8c8098b7ac236338b3619d2c6cf963b82fc

--- device.mk
+++ device.mk
@@ -182,6 +182,10 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     vendor.display.config@1.1
 
+# Display Device Config
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
+
 # Doze
 PRODUCT_PACKAGES += \
     XiaomiDoze
