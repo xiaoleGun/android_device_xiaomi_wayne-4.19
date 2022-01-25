a98b8a0af71d7d791d82866b0f03a083b72a6c5c
Josh Wu <joshwu@google.com>
2022-01-24 22:56:14 -0800

    wayne: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7

--- device.mk
+++ device.mk
@@ -39,7 +39,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.3-impl:32 \
     audio.primary.sdm660 \
     audio.r_submix.default \
@@ -78,6 +77,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
+    android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default
 
 # Boot animation
