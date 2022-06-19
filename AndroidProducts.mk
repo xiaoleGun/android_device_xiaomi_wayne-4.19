e8d3ae9d36d6d670ba0f86a128d5d1815f464407
xiaoleGun <1592501605@qq.com>
2022-06-19 13:21:51 +0800

    wayne: Kaleidoscope is a dream
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_wayne.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_wayne-user \
-    lineage_wayne-userdebug \
-    lineage_wayne-eng
+    $(LOCAL_DIR)/kscope_wayne.mk
