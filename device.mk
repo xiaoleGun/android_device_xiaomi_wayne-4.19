Diva-Room/Miku_device_xiaomi_wayne

wayne: vibrator: Add excluded-input-devices.xml
Signed-off-by: xiaoleGun <1592501605@qq.com>

device.mk
@@ -427,6 +427,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/excluded-input-devices.xml:vendor/etc/excluded-input-devices.xml
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \

xiaoleGun/android_device_xiaomi_wayne-4.19

device.mk
@@ -407,10 +407,13 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
-    
+
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/excluded-input-devices.xml:vendor/etc/excluded-input-devices.xml
 
 # Wifi
 PRODUCT_PACKAGES += \