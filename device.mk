d50a319200b77a17dbdcfb346c02cdb0d853833f
xiaoleGun <1592501605@qq.com>
2022-11-18 18:46:23 +0800

    wayne: Move back to 1.2 USB HAL
    Change-Id: I7ca43c4dd90fd7b6adeaf43254cebb46077b34ce
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- device.mk
+++ device.mk
@@ -381,7 +381,7 @@ PRODUCT_SHIPPING_API_LEVEL := 27
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb@1.2-service-qti
 
 # Vibrator
 PRODUCT_PACKAGES += \
