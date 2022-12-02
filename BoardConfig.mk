33e8fbaf5b2839c0fa2cda61597b20eab4df89f8
xiaoleGun <1592501605@qq.com>
2022-12-02 20:20:41 +0800

    wayne: Decleare dt2w node
    Change-Id: If8eac90a8068d9a55d961cb438d79302fb240c52
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,6 +56,9 @@ TARGET_USES_QTI_MAPPER_EXTENSIONS_1_1 := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
+# DT2W
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
