08b7061857d5ceebe5082f461e18c840e7113cf4
xiaoleGun <1592501605@qq.com>
2022-12-02 20:11:16 +0800

    wayne: Initial sepolicy
    Co-authored-by: Edwin Moquete <edwinmmoquete@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>
    Co-authored-by: Davide Garberi <dade.garberi@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>
    Change-Id: I07278161e4fdef88adb9c576bb4faf77db1fa484
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,6 +121,9 @@ VENDOR_SECURITY_PATCH := 2018-11-01
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Treble
 BOARD_VNDK_VERSION := current
