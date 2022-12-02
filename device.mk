771534846cabb74abfde6ade6bbe580baa6e3318
xiaoleGun <1592501605@qq.com>
2022-12-02 19:36:11 +0800

    wayne: Enable OSS camera
    Change-Id: I6b80298e11d9a964002c913aa39e1f318a08e636
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- device.mk
+++ device.mk
@@ -92,6 +92,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.6:64 \
+    camera.sdm660 \
     libdng_sdk.vendor \
     vendor.qti.hardware.camera.device@1.0:64
 
