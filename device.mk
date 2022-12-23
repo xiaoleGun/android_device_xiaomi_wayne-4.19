574ccc04a767164c408f19c300d20c537cb16b47
xiaoleGun <1592501605@qq.com>
2022-12-23 14:30:17 +0800

    wayne: Import XiaomiDoze
    * From https://github.com/LineageOS/android_device_xiaomi_sdm660-common
    * Get HEAD: fa0942148e5edb0a5c59af515cb33e0b9eba1747
    
    Change-Id: Iaf279f6997aa46e6fd81dd0163f24c574ee27145
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- device.mk
+++ device.mk
@@ -139,6 +139,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/others/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
 
+# Doze
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
