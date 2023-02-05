b55f09667e1f5682faaabf7afca8cd076007a0df
xiaoleGun <1592501605@qq.com>
2023-02-05 21:49:19 +0800

    wayne: Update fingerprint to stock latest
    Change-Id: Iec780f57a71818ab1e9fbbe1acff3afb33938f6d
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- lineage_wayne.mk
+++ lineage_wayne.mk
@@ -25,4 +25,4 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := xiaomi/wayne/wayne:8.1.0/OPM1.171019.011/V9.5.11.0.ODCCNFA:user/release-keys
+BUILD_FINGERPRINT := xiaomi/wayne/wayne:9/PKQ1.180904.001/V12.0.2.0.PDCCNXM:user/release-keys
